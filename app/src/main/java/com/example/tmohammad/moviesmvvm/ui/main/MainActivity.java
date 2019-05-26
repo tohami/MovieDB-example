d80916104604b7249c79bde6927516e8e018f163
tohami <tohami1993@gmail.com>
2019-05-26 15:21:23 +0200

    add navigation component

--- app/src/main/java/com/example/tmohammad/moviesmvvm/ui/main/MainActivity.java
+++ app/src/main/java/com/example/tmohammad/moviesmvvm/ui/main/MainActivity.java
@@ -11,13 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        DataBindingUtil.setContentView(this, R.layout.main_activity);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
+        setContentView(R.layout.main_activity);
     }
 }
