ff4ba8f7f1ac58d5796893b112076a018754514f
tohami <tohami1993@gmail.com>
2019-05-28 04:02:35 +0300

    update ui

--- app/src/main/java/com/example/tmohammad/moviesmvvm/ui/adapter/BindingAdapters.java
+++ app/src/main/java/com/example/tmohammad/moviesmvvm/ui/adapter/BindingAdapters.java
@@ -3,12 +3,21 @@ package com.example.tmohammad.moviesmvvm.ui.adapter;
 import android.databinding.BindingAdapter;
 import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 
 import com.squareup.picasso.Picasso;
 
 public class BindingAdapters {
     @BindingAdapter({"imageUrl", "error"})
     public static void loadImage(ImageView view, String url, Drawable error) {
+        //w300_and_h450_bestv2
+        //w300
+        //w600_and_h900_bestv2
         Picasso.get().load("https://image.tmdb.org/t/p/w300" + url).error(error).into(view);
     }
+
+//    @BindingAdapter({"rating"})
+//    public static void setRating(RatingBar view, double rate) {
+//        view.setRating((float) rate);
+//    }
 }
